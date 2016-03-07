7c62ca556603718edba21927c5458ed1838825a7
qiutao <tauchiou@outlook.com>
2016-03-07 23:57:05 +0800

    add some comments

--- shell/ftp_open.sh
+++ shell/ftp_open.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# obtain host IP adress, inet excluding local host and ipv6
 host_ip="`/sbin/ifconfig -a|grep inet|grep -v 127.0.0.1|grep -v inet6|awk '{print $2}'|tr -d 'addr:'`"
 
 `sudo -s launchctl load -w /System/Library/LaunchDaemons/ftp.plist`
@@ -7,10 +8,11 @@ host_ip="`/sbin/ifconfig -a|grep inet|grep -v 127.0.0.1|grep -v inet6|awk '{prin
 ret=$?
 
 if [ ${ret} -ne 0 ]; then
-	echo "abnormal!"
+	echo "abnormal![${ret}]"
 	exit -1
 fi
 
 echo "ftp server established successfully! you can connect it at ${host_ip}"
 
 echo "to close ftp, run ~/shell/ftp_close.sh"
+
