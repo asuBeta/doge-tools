3aec3eb982d63fe525977fbf3e452e13ffa7c824
asuBeta <tauchiou@outlook.com>
2016-04-04 01:09:23 +0800

    add time functions

--- c_util/w9099s01/w9099s01.c
+++ c_util/w9099s01/w9099s01.c
@@ -10,8 +10,75 @@
 #include <string.h>
 #include <math.h>
 #include <float.h>
+#include <time.h>
+#include <sys/time.h>
 
 #include "w9099s01.h"
+
+
+/**
+* <p>Get system time with precison of millsecond</p>
+* <p>date format: yyyymmddHHMMSSsss(length of 17 + 1)</p>
+*/
+
+void w9099s01_sysdt(char *date){
+    
+    struct timeval tv;
+    
+    struct tm *p_date = NULL;
+    struct timezone *p_tz = NULL;
+    
+    memset(&tv, 0x00, sizeof(struct timeval));
+    gettimeofday(&tv, p_tz);
+    
+    time_t result = tv.tv_sec;
+    
+    if (result){
+        // the date is based on local timezone
+        p_date = localtime(&result);
+        if (p_date){
+            // foramt util second
+            strftime(date, DATE_LEN_14, "%Y%m%d%H%M%S",p_date);
+            // append millsecond to the date string, at the positon of second
+            sprintf(date + DATE_LEN_14, "%03d", tv.tv_usec/1000);
+        }
+    }
+
+}
+
+
+/**
+* <p>Add seconds to or subtract seconds from given time.</p>
+* <p>char *new_dt: new time after operation.</p>
+* <p>const char* old_dt: old time for operation.</p>
+* <p><b>time format(I/O):yyyymmddHHMMSS (length of 14 + 1)</b></p>
+* <p>int second: seconds to add or subtract. Positive for adding,
+*    and negative for subtracting.</p>
+*/
+
+void w9099s01_add_times(char *new_dt, const char* old_dt, int second){
+    
+    struct tm s_old_dt;
+    struct tm *p_new_dt = NULL;
+    
+    time_t total_secs = 0;
+    memset(&s_old_dt, 0x00, sizeof(s_old_dt));
+
+    
+    strptime(old_dt, "%Y%m%d%H%M%S", &s_old_dt);
+    
+    total_secs = mktime(&s_old_dt);
+    
+    if (total_secs) {
+        total_secs += second;
+        p_new_dt = localtime(&total_secs);
+        if (p_new_dt){
+            // foramt util second
+            strftime(new_dt, DATE_LEN_14, "%Y%m%d%H%M%S",p_new_dt);
+        }
+    }
+    
+}
 
 
 /**
@@ -73,13 +140,13 @@ void w9099s01_trim(char *str){
 
 /**
 * <p>A function for high precision multiplication of two float numbers.</p>
-* <li>input: two strings of float numbers, eg, 999.9999</li>
-* <li>output: the multiplication result of inputs in long doule type</li>
+* <li>parameters: two strings of float numbers, eg, 999.9999</li>
+* <li>return: the multiplication result of inputs in long doule type</li>
 */
 
 long double w9099s01_flt_mltply(char* num1, char* num2){
     
-    // variable declares
+    // declare variables
     int sgn_flg = 1;    // sign flag for result
     
     long double ld_x1y1 = 0.0;
--- c_util/w9099s01/w9099s01.h
+++ c_util/w9099s01/w9099s01.h
@@ -8,16 +8,31 @@
 #ifndef w9099s01_h
 #define w9099s01_h
 
+// macro definitons
 #define W9099S01_SWAP(x,y) \
         (x) = (x)^(y);\
         (y) = (x)^(y);\
         (x) = (x)^(y);                  // swap two numbers
 
-#define DOT "."                         // a dot
+
+// const definitons
+#define SYSERR          -1              // system error
+#define DATE_LEN_14    14               // date length of yyyymmddhhmmss
+#define DOT             "."             // a dot
+
+
+// extern function declarations
+extern void w9099s01_sysdt(char *date);
+
+extern void w9099s01_add_times(char *new_dt, const char* old_dt, int second);
 
 extern void w9099s01_ltrim(char *str);
+
 extern void w9099s01_rtrim(char *str);
+
 extern void w9099s01_trim(char *str);
+
 extern long double w9099s01_flt_mltply(char* str1, char* str2);
+
 
 #endif /* w9099s01_h */
